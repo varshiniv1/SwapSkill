1c48b382ec22e8d8dbc4898264733e53164b5079
VarshiniVen-Lillup <varshiniven@lillup.com>
2026-05-31 00:29:29 -0400

    fix: use async pre-save hook without next for Mongoose 9 compatibility

--- server/models/User.js
+++ server/models/User.js
@@ -13,10 +13,9 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 userSchema.methods.matchPassword = function (plain) {
